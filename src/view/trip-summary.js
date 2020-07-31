d42d8125cb107ca384397100b83f75fdd1923d1a
Anton Churkin <afrg@yandex.ru>
2020-07-31 13:48:57 +0500

    main.js is smashed into views

---
+++ src/view/trip-summary.js
@@ -0,0 +1,6 @@
+export const createTripSummaryTemplate = () => {
+  return (
+    `<section class="trip-main__trip-info  trip-info">
+    </section>`
+  );
+};
---
+++ src/view/trip-tabs.js
@@ -0,0 +1,9 @@
+export const createTripTabsTemplate = () => {
+  return (
+    `<h2 class="visually-hidden">Switch trip view</h2>
+    <nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
