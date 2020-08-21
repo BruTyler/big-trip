ac9889fdb4747c0332d5c7a20941f33a328f2dda
Anton Churkin <afrg@yandex.ru>
2020-08-21 21:55:03 +0500

    SmartView is ready

---
+++ src/view/smart.js
@@ -0,0 +1,40 @@
+import AbstractView from './abstract.js';
+import {extend} from '../utils/common.js';
+
+export default class Smart extends AbstractView {
+  constructor() {
+    super();
+    this._data = {};
+  }
+
+  updateData(update, justDataUpdating) {
+    if (!update) {
+      return;
+    }
+
+    this._data = extend(this._data, update);
+
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+
+  updateElement() {
+    let prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+    prevElement = null;
+
+    this.restoreHandlers();
+  }
+
+  restoreHandlers() {
+    throw new Error(`Abstract method not implemented: resetHandlers`);
+  }
+}
