795335a8155ba0510d77863279bacb86e924ae4a
Anton Churkin <afrg@yandex.ru>
2020-09-04 11:26:29 +0500

    fix: sorter hides day label

--- src/view/event-sorter.js
+++ src/view/event-sorter.js
@@ -1,8 +1,9 @@
-import AbstractView from '../abstract/simple-view.js';
+import SmartView from '../abstract/smart-view.js';
 import {capitilizeFirstLetter} from '../utils/common.js';
 import {SortType} from '../const.js';
 
 const createEventSorterTemplate = (selectedSortType) => {
+  const dayLabel = selectedSortType === SortType.EVENT ? `Day` : ``;
   const sortItemsTemplate = Object
     .values(SortType)
     .map((sortItem) =>
@@ -20,24 +21,28 @@ const createEventSorterTemplate = (selectedSortType) => {
 
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+      <span class="trip-sort__item  trip-sort__item--day">${dayLabel}</span>
       ${sortItemsTemplate}
       <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
     </form>`
   );
 };
 
-export default class EventSorter extends AbstractView {
+export default class EventSorter extends SmartView {
   constructor(initSortType) {
     super();
 
-    this._initSortType = initSortType;
+    this._currentSortType = initSortType;
 
     this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createEventSorterTemplate(this._initSortType);
+    return createEventSorterTemplate(this._currentSortType);
+  }
+
+  restoreHandlers() {
+    this.setSortTypeChangeHandler(this._callback.sortTypeChange);
   }
 
   _sortTypeChangeHandler(evt) {
@@ -45,7 +50,9 @@ export default class EventSorter extends AbstractView {
       return;
     }
 
-    this._callback.sortTypeChange(evt.target.value);
+    this._currentSortType = evt.target.value;
+    this.updateElement();
+    this._callback.sortTypeChange(this._currentSortType);
   }
 
   setSortTypeChangeHandler(callback) {
