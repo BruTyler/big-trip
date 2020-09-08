bb2250c7ca60a1d26dfb0a4be190d300c1d557f7
Anton Churkin <afrg@yandex.ru>
2020-09-08 23:51:28 +0500

    adapter for fethed structures is ready

--- src/model/offers.js
+++ src/model/offers.js
@@ -1,7 +1,17 @@
 import SimpleCollection from '../abstract/simple-collection.js';
+import {AdapterDecorator as Fetchable} from '../abstract/fetch-adapter.js';
 
-export default class Offers extends SimpleCollection {
+// eslint-disable-next-line new-cap
+export default class Offers extends Fetchable(SimpleCollection) {
   constructor() {
     super();
+  }
+
+  static adaptToClient(offer) {
+    return offer;
+  }
+
+  static adaptToServer(offer) {
+    return offer;
   }
 }
